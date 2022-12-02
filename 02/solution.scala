390dbc76be042cf6a2d4e9a1286afb46c37abdac
Thomas Chaumeny <t.chaumeny@gmail.com>
2022-12-02 18:26:32 +0100

    Problem 2

---
+++ 02/solution.scala
@@ -0,0 +1,37 @@
+import java.lang.Math.floorMod
+import scala.io.Source
+
+
+object Main {
+    val aliases = List("ABC", "XYZ")
+    def main(args: Array[String]): Unit = {
+        val score1 = computeScore({ case Array(a, b) =>
+            (if (floorMod(b - a, 3) == 1) 6 else if (a == b) 3 else 0) + b + 1
+        })
+        println(s"Score computed with first method: $score1")
+
+        val score2 = computeScore({ case Array(a, b) =>
+            if (b == 0) {
+                floorMod(a - 1, 3) + 1
+            } else if (b == 1) {
+                a + 1 + 3
+            } else {
+                floorMod(a + 1, 3) + 1 + 6
+            }
+        })
+        println(s"Score computed with second method: $score2")
+    }
+    def computeScore(calculator: Array[Int] => Int): Int = {
+        Source
+            .fromFile("input")
+            .mkString
+            .split("\n")
+            .map(_
+                .split(" ")
+                .zipWithIndex
+                .map{ case (letter, player) => aliases(player).indexOfSlice(letter)}
+            )
+            .map(calculator)
+            .sum
+    }
+}
