13da23751e1f8974a35a0ac606da0c9bf3f33b29
Thomas Chaumeny <t.chaumeny@gmail.com>
2022-12-05 17:29:03 +0100

    Refactor problem 5

--- 05/solution.scala
+++ 05/solution.scala
@@ -1,54 +1,43 @@
 import scala.io.Source
+import scala.collection.mutable.Buffer
 
 
-class StacksArrangement(stacks: Array[List[String]]):
-    def message = stacks.flatMap(_.headOption).mkString
-    def rearrange(rearrangement: Rearrangement): StacksArrangement = {
-        val buffer = stacks.toBuffer
-        (1 to rearrangement.quantity) foreach (_ =>
-            buffer(rearrangement.to) +:= buffer(rearrangement.from).head
-            buffer(rearrangement.from) = buffer(rearrangement.from).tail
-        )
-        StacksArrangement(buffer.toArray)
-    }
-    def rearrange2(rearrangement: Rearrangement): StacksArrangement = {
-        val buffer = stacks.toBuffer
-        buffer(rearrangement.to) = buffer(rearrangement.from).take(rearrangement.quantity) ++ buffer(rearrangement.to)
-        buffer(rearrangement.from) = buffer(rearrangement.from).drop(rearrangement.quantity)
-        StacksArrangement(buffer.toArray)
-    }
-end StacksArrangement
-
+type StacksBuffer = Buffer[List[scala.Char]]
 
 case class Rearrangement(quantity: Int, from: Int, to: Int)
 
 
 object Main {
     def main(args: Array[String]) = {
-        println(s"The message on top of the stacks is $problem1")
+        val crateMover9000reducer = (buffer: StacksBuffer, rearrangement: Rearrangement) => {
+            (1 to rearrangement.quantity) foreach (_ =>
+                buffer(rearrangement.to) +:= buffer(rearrangement.from).head
+                buffer(rearrangement.from) = buffer(rearrangement.from).tail
+            )
+            buffer
+        }
+        val problem1 = operate(crateMover9000reducer)
+        println(s"With CrateMover 9000, the message is $problem1")
+        val crateMover9001reducer = (buffer: StacksBuffer, rearrangement: Rearrangement) => {
+            buffer(rearrangement.to) = buffer(rearrangement.from).take(rearrangement.quantity) ++ buffer(rearrangement.to)
+            buffer(rearrangement.from) = buffer(rearrangement.from).drop(rearrangement.quantity)
+            buffer
+        }
+        val problem2 = operate(crateMover9001reducer)
         println(s"With CrateMover 9001, the message is $problem2")
     }
-    def problem1 = {
-        val Array(rawStacks, rawRearrangements) = 
-        Source
-            .fromFile("input")
-            .mkString
-            .split("\n\n")
-        initRearrangements(rawRearrangements)
-            .foldLeft(initStacks(rawStacks))(_ rearrange _)
-            .message
-    }
-    def problem2 = {
+    def operate(reducer: (StacksBuffer, Rearrangement) => StacksBuffer): String = {
         val Array(rawStacks, rawRearrangements) =
         Source
             .fromFile("input")
             .mkString
             .split("\n\n")
         initRearrangements(rawRearrangements)
-            .foldLeft(initStacks(rawStacks))(_ rearrange2 _)
-            .message
+            .foldLeft(initStacks(rawStacks))(reducer(_, _))
+            .flatMap(_.headOption)
+            .mkString
     }
-    def initStacks(rawStacks: String): StacksArrangement = {
+    def initStacks(rawStacks: String): StacksBuffer = {
         val lines =
         rawStacks
             .split("\n")
@@ -56,28 +45,23 @@ object Main {
 
         val stacks = 
         lines(0)
+            .trim
             .split("\\s+")
-            .map(_ => List.empty[String])
+            .map(_ => List.empty[Char])
             .toBuffer
 
-        val cratePattern = "\\[([A-Z])\\]\\s*".r
         lines
             .drop(1)
-            .foreach(_
-                .grouped(4)
-                .map{
-                    case cratePattern(label) => Some(label)
-                    case _ => None
-                }
-                .zipWithIndex
-                .foreach((label, pos) =>
-                    label
-                    .foreach(
-                        stacks(pos) +:= _
-                    )
+            .foreach(line =>
+                (0 to stacks.length - 1)
+                .flatMap(pos =>
+                    Some(line(4 * pos + 1))
+                    .filter(_ != ' ')
+                    .map((pos, _))
                 )
+                .foreach(stacks(_) +:= _)
             )
-        StacksArrangement(stacks.toArray)
+        stacks
     }
     def initRearrangements(rawRearrangements: String): Array[Rearrangement] = {
         val rearrangementPattern = "move (\\d+) from (\\d+) to (\\d+)".r
