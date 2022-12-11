3ac5f1c7dfc2448b869fa97d812685bab01edaa0
Thomas Chaumeny <t.chaumeny@gmail.com>
2022-12-11 16:54:22 +0100

    Problem 11, part 2

--- 11/solution.scala
+++ 11/solution.scala
@@ -4,13 +4,14 @@ import scala.io.Source
 def ints(s: String) = "\\d+".r.findAllIn(s).map(_.toInt)
 
 
-case class Monkey(operation: Int => Int, recipient: Int => Int)
+case class Monkey(operation: Int => Int, recipient: Int => Int, modulo: Int)
 
 
 object Main {
     val operationPattern = "Operation: new = (\\w+) ([+*]) (\\w+)".r
     def main(args: Array[String]) = {
         println(s"The level of monkey business after 20 rounds is $problem1")
+        println(s"The level of monkey business after 10000 rounds is $problem2")
     }
     def initialize: (Array[Monkey], Array[Vector[Int]]) = {
         Source
@@ -19,17 +20,15 @@ object Main {
             .split("\n\n")
             .map(raw =>
                 val lines = raw.linesIterator.drop(1).toVector
+                val modulo = ints(lines(2)).next
                 val operation = (old: Int) => {
                     val parseOpd = (operand: String) => if (operand == "old") old else operand.toInt
                     lines(1).trim match
                         case operationPattern(left, "+", right) => parseOpd(left) + parseOpd(right)
                         case operationPattern(left, "*", right) => parseOpd(left) * parseOpd(right)
                 }
-                val recipient = (old: Int) => {
-                    val divisor = ints(lines(2)).next
-                    ints(lines(if (old % divisor == 0) 3 else 4)).next
-                }
-                (Monkey(operation, recipient), ints(lines(0)).toVector)
+                val recipient = (level: Int) => ints(lines(if (level % modulo == 0) 3 else 4)).next
+                (Monkey(operation, recipient, modulo), ints(lines(0)).toVector)
             )
             .unzip
     }
@@ -54,5 +53,37 @@ object Main {
             //)
         }
         activity.sorted.takeRight(2).reduce(_ * _)
+    }
+    def problem2 = {
+        val (monkeys, initState) = initialize
+        val activity = monkeys.map(_ => 0)
+
+        val state = initState
+            .map(
+                _
+                .map(item =>
+                    monkeys.map(item % _.modulo)
+                )
+            )
+
+        for (round <- 0 until 10_000) {
+            for {
+                turn <- 0 until monkeys.length
+                item <- state(turn)
+            } {
+                val levels = (0 until monkeys.length).map(monkey =>
+                    (monkeys(turn).operation(item(monkey)) % monkeys(monkey).modulo)
+                ).toArray
+                val recipient = monkeys(turn).recipient(levels(turn))
+                state(turn) = state(turn).drop(1)
+                state(recipient) = state(recipient) :+ levels
+                activity(turn) = activity(turn) + 1
+            }
+            //println(s"State after round ${round + 1}:")
+            //state.zipWithIndex.foreach((items, idx) =>
+            //    println(s"$idx: ${items.map(_.mkString(":")).mkString(", ")}")
+            //)
+        }
+        activity.sorted.map(BigInt(_)).takeRight(2).reduce(_ * _)
     }
 }
