e3e2c4637c89da52c53fe17e6678684eccc6ad11
Thomas Chaumeny <t.chaumeny@gmail.com>
2022-12-22 22:41:39 +0100

    Day 16, part 2

--- 16/solution.scala
+++ 16/solution.scala
@@ -9,25 +9,30 @@ object Main {
         .map(valve => valve -> distanceMap(valve))
         .toMap
     def main(args: Array[String]) = {
-        //println(valvesMap)
-        //shortcutsMap
-        //    .foreach((valve, shortcuts) =>
-        //        println(s"$valve:")
-        //        println(shortcuts)
-        //    )
-        println(maxRelease(30, "AA"))
+        println(problem1)
+        println(problem2)
     }
-    def maxRelease(time: Int, from: String, openValves: Set[String] = Set.empty): Int = {
+    def problem1 = maxRelease(30, "AA", valvesMap.filter(_._2._1 != 0).keySet)
+    def problem2 = {
+        val valves = valvesMap.filter(_._2._1 != 0).keySet
+        valves
+            .subsets
+            .map(subset =>
+                maxRelease(26, "AA", subset) + maxRelease(26, "AA", valves diff subset)
+            )
+            .max
+    }
+    def maxRelease(time: Int, from: String, valves: Set[String]): Int = {
         if (time == 0) 0
         else {
-            val releasePerStep = openValves.map(valvesMap(_)._1).sum
-            shortcutsMap(from)
-                .filter((to, steps) => steps + 1 <= time && !openValves(to) && valvesMap(to)._1 != 0)
-                .map((to, steps) =>
-                    (steps + 1) * releasePerStep + maxRelease(time - steps - 1, to, openValves + to)
-                )
-                .maxOption
-                .getOrElse(time * releasePerStep)
+            valves
+            .map(to => (to, shortcutsMap(from)(to)))
+            .filter((to, steps) => steps + 1 <= time)
+            .map((to, steps) =>
+                (time - steps - 1) * valvesMap(to)._1 + maxRelease(time - steps - 1, to, valves - to)
+            )
+            .maxOption
+            .getOrElse(0)
         }
     }
     def initValvesMap = {
@@ -40,7 +45,6 @@ object Main {
             }
             .toMap
     }
-
     def distanceMap(source: String) = {
         @tailrec
         def integrateBatch(batch: List[String], distance: Int, distances: Map[String, Int], seen: Set[String]): Map[String, Int] = {
